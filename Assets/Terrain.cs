c41cb0404d39576f90f3450a18c03a86fc23b85e
bukovszkiaron-creator <bukovszki.aron@gmail.com>
2026-01-16 19:35:06 +0100

    added chunk generation

--- Assets/Terrain.cs
+++ Assets/Terrain.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 public class Terrain : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject blockPrefab;
+
+    public int chunkSize = 32;
+    public int maxHeight = 10;
+
+
+    private void Start()
     {
-        
+        GenerateTerrain();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void GenerateTerrain()
     {
-        
+       int halfChunksSize  = chunkSize / 2;
+
+       for(int x = -halfChunksSize; x < halfChunksSize; x++)
+       {
+           for(int z = -halfChunksSize; z < halfChunksSize; z++)
+            {
+                int totalHeight = maxHeight; // You can modify this to create varied terrain heights
+                for(int y = 0; y > -totalHeight; y--)
+                {
+                Vector3 spawnPos = new Vector3(x, y, z);
+
+                Instantiate(blockPrefab, spawnPos, Quaternion.identity);
+                }
+            }
+
+        }
     }
 }
